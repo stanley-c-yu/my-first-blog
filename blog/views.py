928bf61f148af3cd1dadda310e810322fab5b3d8
Stanley Yu <stanyu88@gmail.com>
2022-08-24 14:28:48 -0400

    Added an HTML Template for blog posts and some urls paths

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
+'''
+Views are where we write the logic of our application. 
+
+They will request information from the model we've created, and pass it to a template.  
+'''
+
 from django.shortcuts import render
 
 # Create your views here.
+def post_list(request):
+    '''
+    Takes a request and returns the value it gets from calling the 'render' function.  It then renders it using our template, blog/post_list.html. 
+    '''
+    return render(request, 'blog/post_list.html', {})
