3b73fda66b5d4a5a1739ee4d62104fe7a43bf606
Stanley Yu <stanyu88@gmail.com>
2022-08-26 13:33:03 -0400

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views # Import our views from the 'blog' application
 # URL patterns for Blog Application 
 urlpatterns = [
     path('', views.post_list, name = 'post_list'), # The 'view' called 'post_list' is assigned too the root URL. This pattern will match an empty string, and the Django URL resolver will ignore the domain name (i.e., http://127.0.0.1:8000/) that prefixes the full URL Path.  It will tell Django that views.post_list is the place to go if someone enters the website at http://127.0.0.1:8000/
+    path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ Views are where we write the logic of our application.
 They will request information from the model we've created, and pass it to a template.  
 '''
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -15,3 +15,7 @@ def post_list(request):
     '''
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+    
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
